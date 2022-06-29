a2b97f45eb15152ab6c243223fb330d429e07578
D.정성준 <seongjun.chung@supercat.co.kr>
2022-06-29 19:11:42 +0900

    기본 구조 잡기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,56 @@
 import styles from "../styles/Home.module.css";
 import { writeMealData, getMealData } from "../utils/firebase";
 import { useQuery, useMutation, useQueryClient } from "react-query";
+import { useState } from "react";
 
 export default function Home({ menuData }) {
+  const [currentMenu, setCurrentMenu] = useState("");
+  const [menuList, setMenuList] = useState([]);
   const queryClient = useQueryClient();
 
-  const query = useQuery("menus", getMenus);
+  // const query = useQuery("menus", getMenus);
 
   // Mutations
-  const mutation = useMutation(postMenu, {
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries("menus");
-    },
-  });
-
-  console.log(menuData);
-
+  // const mutation = useMutation(postMenu, {
+  //   onSuccess: () => {
+  //     // Invalidate and refetch
+  //     queryClient.invalidateQueries("menus");
+  //   },
+  // });
   return (
     <>
-      <label>꼭 오늘의 메뉴를 확실하게 알고 계신 분이 입력해주세요!</label>
-      <input type="text" />
-      <button>추가하기</button>
+      <div>
+        <label>꼭 오늘의 메뉴를 확실하게 알고 계신 분이 입력해주세요!</label>
+        <div>
+          <input
+            type="text"
+            value={currentMenu}
+            onChange={(e) => setCurrentMenu(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              setMenuList([...menuList, currentMenu]);
+              setCurrentMenu("");
+            }}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <h4>입력된 메뉴 : </h4>
+      <ul>
+        {menuList?.map((m, i) => (
+          <li key={i}>{m}</li>
+        ))}
+      </ul>
+      <button onClick={() => writeMealData(new Date(), menuList)}>
+        메뉴 제출하기
+      </button>
 
       <h1>오늘의 메뉴는!?????!?!?!??!?!?</h1>
       <ul>
-        {query.data.map((menu, idx) => (
+        {menuData.map((menu, idx) => (
           <li key={idx}>{menu}</li>
         ))}
       </ul>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,29 +1,30 @@
-import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, set, get, child } from 'firebase/database'
-import dayjs from 'dayjs'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, get, child } from "firebase/database";
+import dayjs from "dayjs";
 
 const initializeFirebase = () => {
   const firebaseConfig = {
-    databaseURL: 'https://auntgivemebob-default-rtdb.asia-southeast1.firebaseio.com'
-  }
-  
-  const app = initializeApp(firebaseConfig)
-  getDatabase(app)
+    databaseURL:
+      "https://auntgivemebob-default-rtdb.asia-southeast1.firebaseio.com",
+  };
 
-  return app
-}
+  const app = initializeApp(firebaseConfig);
+  getDatabase(app);
+
+  return app;
+};
 
 /**
  * 특정 날짜에 메뉴 데이터 쓰기
  * @param {string | Date} date 날짜 '2022-06-29' or Date
  * @param {string[]} menus 입력한 메뉴들
  */
-const writeMealData = (date, menus) => {
-  const db = getDatabase()
-  set(ref(db, dayjs(date).format('YYYYMMDD')), {
+const writeMealData = (date = new Date(), menus) => {
+  const db = getDatabase();
+  set(ref(db, dayjs(date).format("YYYYMMDD")), {
     menus,
-  })
-}
+  });
+};
 
 /**
  * 특정 날짜의 메뉴 가져오기
@@ -31,14 +32,14 @@ const writeMealData = (date, menus) => {
  * @returns {string[]}
  */
 const getMealData = async (date = new Date()) => {
-  const db = getDatabase()
+  const db = getDatabase();
   try {
-    const menus = await get(ref(db, dayjs(date).format('YYYYMMDD')))
+    const menus = await get(ref(db, dayjs(date).format("YYYYMMDD")));
 
-    return menus.exists() ? menus.val()?.menus ?? [] : []
+    return menus.exists() ? menus.val()?.menus ?? [] : [];
   } catch (e) {
-    return []
+    return [];
   }
-}
+};
 
-export { initializeFirebase, writeMealData, getMealData }
+export { initializeFirebase, writeMealData, getMealData };
